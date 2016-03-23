9b2c485196b9bc9d13337b329a2afb16cf79e3ff
MaksimPW <ma.savichev@gmail.com>
2016-03-23 22:52:02 +0600

    Add validate for Model:Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,8 @@
 class Question < ActiveRecord::Base
+	has_many :answers
+	
 	validates :title, :body, presence: true
+
+	validates :title, length:  { in: 15..150 }
+	validates :body, length: { minimum: 30 }
 end
