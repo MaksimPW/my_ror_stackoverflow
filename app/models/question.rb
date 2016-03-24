dc5a21a9ee1cad9958cfcf9875a8ca9ffcbd7774
MaksimPW <ma.savichev@gmail.com>
2016-03-24 16:50:42 +0600

    Add validate for dependent destroy in Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-	has_many :answers
+	has_many :answers, dependent: :destroy
 	validates :title, presence: true, length: { in: 15..150  }
 	validates :body, presence: true, length: { minimum: 30 }
 end
