a8d886065d78cade68e0c4df2be743304bbbf1f3
MaksimPW <ma.savichev@gmail.com>
2016-03-27 22:51:55 +0600

    4-task | Add tests for Questions#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -7,13 +7,29 @@ RSpec.describe QuestionsController, type: :controller do
 		before do
 			get :index
 		end
-		
+
 		it 'pupulates an array of all questions' do
 			expect(assigns(:questions)).to match_array(questions)
 		end
 
 		it 'render index view' do
 			expect(response).to render_template :index
+		end
+	end
+
+	describe 'GET #show' do
+		let(:question) { create(:question) }
+
+		before do
+			get :show, id: question
+		end
+		
+		it 'assigns the requested question to @question' do
+			expect(assigns(:question)).to eq question
+		end
+
+		it 'renders show view' do
+			expect(response).to render_template :show
 		end
 	end
 end
