b0b248abf20bde39ec81ac084f5f2269fd7305a3
MaksimPW <ma.savichev@gmail.com>
2016-03-28 00:09:03 +0600

    4-task | Update Questions#update for redirect_to at invalid_params

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -26,7 +26,11 @@ class QuestionsController < ApplicationController
 
 	def update
 		@question.update(question_params)
-		redirect_to @question
+		if @question.save
+			redirect_to @question
+		else
+			render :edit
+		end
 	end
 
 	private
