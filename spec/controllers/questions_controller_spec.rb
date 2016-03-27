64e33aa139717b47f5eb994d6185e8af1fcc5a3a
MaksimPW <ma.savichev@gmail.com>
2016-03-27 23:40:55 +0600

    4-task | Add tests which FactoryGirl.invalid_params for Questions#new

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -64,5 +64,16 @@ RSpec.describe QuestionsController, type: :controller do
 				expect(response).to redirect_to question_path(assigns(:question))
 			end
 		end
+
+		context 'with invalid attributes' do
+			it 'saves the new question in the database' do
+				expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+			end
+
+			it 're-renders new view' do
+				post :create, question: attributes_for(:invalid_question)
+				expect(response).to render_template :new
+			end
+		end
 	end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -3,4 +3,9 @@ FactoryGirl.define do
     title "Good title for Question"
     body "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam"
   end
+
+  factory :invalid_question, class: "Question" do
+  	title nil
+  	body nil
+  end
 end
