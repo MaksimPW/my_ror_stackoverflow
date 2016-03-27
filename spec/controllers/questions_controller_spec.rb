9dd5c49abb7ba4e3bdfac6928f2cef32db2b6cad
MaksimPW <ma.savichev@gmail.com>
2016-03-27 22:59:02 +0600

    4-task | Refactoring before in tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #index' do
 		let(:questions) {create_list(:question, 2)}
 		
-		before do
-			get :index
-		end
+		before { get :index }
 
 		it 'pupulates an array of all questions' do
 			expect(assigns(:questions)).to match_array(questions)
@@ -20,10 +18,8 @@ RSpec.describe QuestionsController, type: :controller do
 	describe 'GET #show' do
 		let(:question) { create(:question) }
 
-		before do
-			get :show, id: question
-		end
-		
+		before { get :show, id: question }
+
 		it 'assigns the requested question to @question' do
 			expect(assigns(:question)).to eq question
 		end
