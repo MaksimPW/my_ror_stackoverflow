3f2a040ba3c0ea7498a1dba5e46ea5aed610cfa3
MaksimPW <ma.savichev@gmail.com>
2016-03-28 20:51:22 +0600

    4-task | Add tests for Answers#new

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+	let(:question) { create(:question) }
+	let(:answer) { create(:answer, question: question) }
+
+	describe 'GET #new' do
+		before { get :new, question_id: question.id }
+
+		it 'assigns a new Answer to @answer' do
+			expect(assigns(:answer)).to be_a_new(Answer)
+		end
+
+		it 'renders new view' do
+			expect(response).to render_template :new
+		end
+	end
+
+	describe 'POST #create' do
+	end
 
 end
