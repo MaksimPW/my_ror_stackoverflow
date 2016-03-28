0f32739da2a9f2acd14400e035fb487882b759f4
MaksimPW <ma.savichev@gmail.com>
2016-03-28 21:45:57 +0600

    4-task | Add test for Answers#create and invalid_answer factory

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -17,6 +17,25 @@ RSpec.describe AnswersController, type: :controller do
 	end
 
 	describe 'POST #create' do
-	end
+		context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect { post :create, question_id: question.id, answer: attributes_for(:answer) }.to change(question.answers, :count).by(1)
+      end
 
+			it 'redirects to show view' do
+				post :create, question_id: question, answer: attributes_for(:answer)
+				expect(response).to redirect_to question_path(assigns(:question))
+			end
+		end
+
+		context 'with invalid attributes' do
+			it 'saves the new question in the database' do
+				expect { post :create, question_id: question.id, answer: attributes_for(:invalid_answer) }.to_not change(question.answers, :count)
+			end
+
+			it 're-renders new view' do
+				post :create, question_id: question, answer: attributes_for(:invalid_answer)
+			end
+		end
+	end
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :answer do
-    body "MyText"
+    body "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam"
+  end
+
+  factory :invalid_answer, class: "Answer" do
+  	body nil
   end
 end
