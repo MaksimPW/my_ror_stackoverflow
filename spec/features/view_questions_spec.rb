b57016557fc723b8f495e1207276734087551fe7
MaksimPW <ma.savichev@gmail.com>
2016-04-10 22:32:55 +0600

    5-task | User can view list answers in question show

--- spec/features/view_questions_spec.rb
+++ spec/features/view_questions_spec.rb
@@ -3,12 +3,24 @@ require 'rails_helper'
 feature 'User || Guest can view' do
   given(:questions) { create_list :question, 3 }
   given!(:question) { questions.first }
+  given(:answers) { create_list(:answer, 3, question: question) }
 
   before { visit root_path }
 
   scenario "User can view list questions in Index" do
     questions.each do |question|
       expect(page).to have_link question.title, href: question_path(question)
+    end
+  end
+
+  scenario "User can view list answers in question show" do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    answers.each do |answer|
+      expect(page).to have_content answer.body
     end
   end
 end
